6ae381e9935247163c145dbc69555bdd553076fc
Guillaume <hi@255kb.dev>
2021-09-22 15:38:49 +0200

    Todo update

--- Day 2/todo/todo.js
+++ Day 2/todo/todo.js
@@ -1,25 +1,30 @@
-const taskList = ["old task 1", "old task 2"];
+const taskList = ["old task 1", "old task 2"]; // maybe coming from a database
+const tasksElement = document.getElementById("list");
 
-const displayTasks = function () {
-  const tasksElement = document.getElementById("list");
-
-  tasksElement.innerHTML = "";
-
+const initList = function () {
   for (let taskIndex = 0; taskIndex < taskList.length; taskIndex++) {
-    const task = document.createElement("li");
-    task.innerText = taskList[taskIndex];
-    tasksElement.appendChild(task);
+    appendTaskElement(taskIndex);
   }
 };
 
-displayTasks();
+const appendTaskElement = function (index) {
+  console.log("call with index ", index);
+  
+  const task = document.createElement("li");
+  task.innerText = taskList[index];
+  task.style = red
+  tasksElement.appendChild(task);
+};
 
 const addTask = function () {
+  // alternative to select the input
   // const taskName = document.getElementsByTagName("input")[0].value;
   const taskName = document.querySelector('input[name="taskName"]').value;
 
   taskList.push(taskName);
   console.log(taskList);
 
-  displayTasks();
+  appendTaskElement(taskList.length - 1);
 };
+
+initList();
